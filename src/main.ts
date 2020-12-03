5c93f76cf27355d643212966b34936fed46c00fd
Will Bartlett <will118@gmail.com>
2020-12-03 22:25:45 +0000

    Deletes sample code

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,35 @@
-// Main Game Logic
-import * as ex from 'excalibur';
-import Config from './config';
-import { Sounds, loader } from './resources';
-import { Game } from './game';
+import * as ex from "excalibur";
 
-const engine = new ex.Engine({
-    backgroundColor: ex.Color.Black
-});
-engine.backgroundColor = ex.Color.Black;
-engine.setAntialiasing(false);
-
-// Setup game scene
-engine.add('game', new Game(engine));
-engine.goToScene('game');
-
-// Game events to handle
-engine.on('hidden', () => {
-    console.log('pause');
-    engine.stop();
-});
-engine.on('visible', () => {
-    console.log('start');
-    engine.start();
+const game = new ex.Engine({
+  height: 768,
+  width: 1024
 });
 
-engine.input.keyboard.on('press', (evt: ex.Input.KeyEvent) => {
-    if (evt.key === ex.Input.Keys.D) {
-      engine.isDebug = !engine.isDebug;
-    }
+const ship = new ex.Actor({
+  x: 150,
+  y: game.drawHeight / 2,
+  width: 200,
+  height: 200
 });
 
-engine.start(loader).then(() => {
-   Sounds.laserSound.volume = Config.soundVolume;
-   Sounds.explodeSound.volume = Config.soundVolume;
-   Sounds.enemyFireSound.volume = Config.soundVolume;
-   Sounds.powerUp.volume = Config.soundVolume;
-   Sounds.rocketSound.volume = Config.soundVolume;
+ship.color = ex.Color.Chartreuse;
+ship.body.collider.type = ex.CollisionType.Fixed;
 
-   console.log("Game Resources Loaded");
-});
+const bg = new ex.Texture('/assets/sprites/space/BlueNebulae.png');
+const loader = new ex.Loader([bg]);
+
+const map = new ex.TileMap(0, 0, 960, 480, 20, 20);
+const sheet = new ex.SpriteSheet(bg, 1, 1, 960, 540);
+map.registerSpriteSheet('sheet5000', sheet);
+const ts = new ex.TileSprite('sheet5000', 0);
+map.getCell(0, 0).pushSprite(ts);
+map.getCell(0, 1).pushSprite(ts);
+map.getCell(1, 1).pushSprite(ts);
+map.getCell(1, 0).pushSprite(ts);
+
+game.start(loader).then(() => {
+  game.addTileMap(map);
+  game.add(ship);
+  ship.vel.setTo(100, 0);
+  game.currentScene.camera.strategy.lockToActor(ship);
+})
